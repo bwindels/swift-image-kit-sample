f67c1784154aad4d8831920db4eea7ec7fe65373
Bruno Windels <bwindels@Brunos-MacBook-Pro.local>
2015-03-13 23:45:04 +0100

    Initial Commit

---
+++ pictures2/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  pictures2
+//
+//  Created by Bruno Windels on 13/03/15.
+//  Copyright (c) 2015 Bruno Windels. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ pictures2Tests/pictures2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  pictures2Tests.swift
+//  pictures2Tests
+//
+//  Created by Bruno Windels on 13/03/15.
+//  Copyright (c) 2015 Bruno Windels. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class pictures2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
